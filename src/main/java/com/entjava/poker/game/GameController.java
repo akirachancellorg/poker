460a28892c3bf05473146843752fb1bd766d5283
Jorenzo Martin Reyes <jjreyes@student.apc.edu.ph>
2024-11-06 23:48:15 +0800

    added 2 endpoints

--- src/main/java/com/entjava/poker/game/GameController.java
+++ src/main/java/com/entjava/poker/game/GameController.java
@@ -4,18 +4,49 @@ import com.entjava.poker.card.BlankCard;
 import com.entjava.poker.card.Card;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class GameController {
 
 	private Game game;
 
 	public GameController(Game game) {
 		this.game = game;
+	}
+
+	@GetMapping("/getPlayers")
+	@ResponseBody
+	public List<Player> player()
+	{
+		return game.getPlayers();
+	}
+
+	@PostMapping("/start_game")
+	@ResponseBody
+	public void startGame(@RequestBody List<Player> playerInput) {
+
+		List<Player> registeredPlayers = game.getPlayers();
+		List<String> playerNames = new ArrayList<>();
+
+		for (Player player : registeredPlayers) {
+			playerNames.add(player.getName());
+		}
+
+		for (Player player : playerInput) {
+			if (playerNames.contains(player.getName())) {
+				System.out.println(player + " is registered.");
+			}
+			else
+			{
+				System.out.println(player + " is not registered.");
+			}
+		}
 	}
 
 	@GetMapping("/")
--- src/main/java/com/entjava/poker/game/Player.java
+++ src/main/java/com/entjava/poker/game/Player.java
@@ -14,6 +14,7 @@ public class Player {
 	private String name;
 	private List<Card> hand = new ArrayList<>();
 
+	public Player() {};
 	public Player(String name) {
 		this.name = name;
 	}
